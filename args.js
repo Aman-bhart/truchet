4a30236558ab34ea8b8d4fea181ae426e109902c
amanKumarBharti <183721382+Aman-bhart@users.noreply.github.com>
2025-01-04 18:25:03 +0530

    #making the generatedTiles randomly

---
+++ args.js
@@ -0,0 +1,3 @@
+const a = Deno.args;
+
+console.log(a);
--- truchetMain.js
+++ truchetMain.js
@@ -1,4 +1,4 @@
-import { generateTiles } from './truchet.js';
+import { generateTiles } from "./truchet.js";
 import {
   borderLands,
   circle,
@@ -11,13 +11,26 @@ import {
   diagonally,
   zag,
   zig,
-} from './tileGenerators.js';
+} from "./tileGenerators.js";
 
 // Vary this. Try any or all of the above
 // Also try each algorithm with a variety of widths and heights
 // For example: const tileGenerator = halfAndHalf;
-const tileGenerator = classicTruchet;
+const randomTiles = [
+  borderLands,
+  circle,
+  classicAlternate,
+  classicSquare,
+  classicTruchet,
+  gradually,
+  halfAndHalf,
+  halfAndHalfInverse,
+  diagonally,
+  zag,
+  zig,
+];
+const tileGenerator = randomTiles.at(Math.floor(Math.random() * 10));
 
-const [width, height] = Deno.args.map(x => +x);
+const [width, height] = Deno.args.map((x) => +x);
 
 console.log(generateTiles(width, height, tileGenerator));
